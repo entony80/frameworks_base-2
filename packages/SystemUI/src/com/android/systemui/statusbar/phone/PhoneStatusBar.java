fb0aa49cd891d01b319a5cd51843793fba81090b
Varun Date <date.varun123@gmail.com>
2016-04-28 18:13:00 +0000

    Pulse Bar Customizations [1/3]
    Change-Id: I70427dc357290b0893d6d27590ca136ffa30f262
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -634,9 +634,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_CUSTOM_HEADER_SHADOW), false, this,
                     UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.PULSE_CUSTOM_DIMEN),
-                    false, this, UserHandle.USER_ALL);
             update();
         }
 
