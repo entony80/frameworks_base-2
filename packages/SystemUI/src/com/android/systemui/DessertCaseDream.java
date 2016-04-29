d7e1b480bb92ee0f39d91d6bede9eae3844e8e02
Sirhc95 <chriscrawf95@gmail.com>
2016-04-29 12:58:19 -0700

    .

---
+++ packages/SystemUI/src/com/android/systemui/DessertCaseDream.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui;
+
+import android.service.dreams.DreamService;
+
+public class DessertCaseDream extends DreamService {
+    private DessertCaseView mView;
+    private DessertCaseView.RescalingContainer mContainer;
+    private final Runnable mRunnable = new Runnable() {
+        public void run() {
+            DessertCaseDream.this.mView.start();
+        }
+    };
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        setInteractive(false);
+
+        mView = new DessertCaseView(this);
+
+        mContainer = new DessertCaseView.RescalingContainer(this);
+
+        mContainer.setView(mView);
+
+        setContentView(mContainer);
+    }
+
+    @Override
+    public void onDreamingStarted() {
+        super.onDreamingStarted();
+        mView.postDelayed(mRunnable, 1000);
+    }
+
+    @Override
+    public void onDreamingStopped() {
+        super.onDreamingStopped();
+        mView.removeCallbacks(mRunnable);
+        mView.stop();
+    }
+}
